adda84a1a13b425fc922a50ad5fb78f9fb14edaa
Rufuz42 <Steven.Rufus42@gmail.com>
2022-10-02 00:51:00 -0400

    most of the earthquake, all of the fault lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -53,22 +53,100 @@ var map = L.map("map", {
 // Stadia_AlidadeSmoothDark.addTo(map);
 
 // Layer controls
-L.control
-    .layers(baseMaps)
-    .addTo(map);
-
-// Layer controls
 // L.control
-//     .layers(baseMaps, {
-//         collapsed: false
-//     }).addTo(map);
+//     .layers(baseMaps)
+//     .addTo(map);
 
-// L.control.layers(baseMaps, {
-//     collapsed: false
-// }).addTo(map);
-
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(map);
 
 // Tectonic plate call
+var tectonicPlates = new L.layerGroup();
+
+// Call the API for tectonic plate info
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
+.then(function(plateData){
+    // Console log to see if the data loads up
+    // console.log(plateData)
+
+    // load the data with geojson
+    L.geoJson(plateData, {
+        // Add style to see the lines
+        color: "orange",
+        weight: 1
+    }).addTo(tectonicPlates);
+});
+
+// Add tectonic plates to the map
+tectonicPlates.addTo(map);
+
+// Earthquake overlay info
+var earthquakes = new L.layerGroup();
+
+// Get earthquake data
+// Call the API
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
+.then(
+    function(earthquakeData){
+        // Make sure the earthquake data loads
+        // console.log(earthquakeData);
+
+        // Make circles that reflects radius = mag, color = depth
+        // Choose the color function
+        function colorChoice(depth){
+            if (depth > 90)
+                return "red";
+            else if (depth > 70)
+                return "orange";
+            else if (depth > 50)
+                return "light orange"
+            else if (depth > 30)
+                return "yellow"
+            else if (depth > 10)
+                return "light green";
+            else 
+                return "green"
+        }
+
+        // Determine radius size
+        function radiusSize(mag){
+            if (mag == 0)
+                return 1;
+            else
+                return mag * 3;
+        }
+
+        // Style the earthquake circles on the map
+        function style(feature){
+            return {
+                opacity: 0.75,
+                fillOpacity: 0.75,
+                fillColor: colorChoice(feature.geometry.coordinates[2]),
+                color: "black",
+                radius: radiusSize(feature.properties.mag),
+                weight: 0.5
+            }
+        }
+
+        // add geojson to earthquake layers
+        L.geoJson(earthquakeData, {
+            pointToLayer: function(feature, latLng) {
+                return L.circleMarker(latLng);
+            },
+            // Set the marker styles
+            style: style,
+            // Popups
+
+        }).addTo(earthquakes);
+    }
+)
+
+earthquakes.addTo(map);
+
+// Overlap to allow for tectonic plate toggling
+var overlayMaps = {
+    "Tectonic Plates": tectonicPlates,
+    Earthquakes: earthquakes
+};
+
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(map);
